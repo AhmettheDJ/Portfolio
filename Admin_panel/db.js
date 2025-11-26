3689522389488b8a1bd935f4dfa128edafbff46b
AhmettheDJ <155665376+AhmettheDJ@users.noreply.github.com>
2025-11-26 18:36:03 +0100

    Dummy Admin Panel

---
+++ Admin_panel/db.js
@@ -0,0 +1,17 @@
+//dbname = "firma"
+//table = "mitarbeiter"
+
+let workers = [
+    { id: 1, name: 'Ahmet Oezturk', role: 'Administrator', email: 'o.ahmet@example.com', salary: 2500.00, gender: 'male', hire_date: '2025-10-01 15:33:16' },
+    { id: 2, name: 'John Doe', role: 'Designer', email: 'J.Doe@example.com', salary: 2200.00, gender: 'male', hire_date: '2025-10-05 21:54:04' },
+    { id: 3, name: 'Lisa Müller', role: 'Managerin', email: 'l.mueller@firma.com', salary: 3200.00, gender: 'female', hire_date: '2023-03-01 09:00:00' },
+    { id: 4, name: 'Thomas Schneider', role: 'IT-Administrator', email: 't.schneider@firma.com', salary: 3100.00, gender: 'male', hire_date: '2022-07-15 08:30:00' },
+    { id: 5, name: 'Julia Fischer', role: 'Recruiterin', email: 'j.fischer@firma.com', salary: 3000.00, gender: 'female', hire_date: '2024-03-01 09:00:00' },
+    { id: 6, name: 'Michael Weber', role: 'Softwareentwickler', email: 'm.weber@firma.com', salary: 3500.00, gender: 'male', hire_date: '2023-11-20 10:00:00' },
+    { id: 7, name: 'Anna Mayer', role: 'Frontend Developer', email: 'a.mayer@firma.com', salary: 3300.00, gender: 'female', hire_date: '2025-01-12 09:15:00' },
+    { id: 8, name: 'Daniel Berger', role: 'System Engineer', email: 'd.berger@firma.com', salary: 3400.00, gender: 'male', hire_date: '2022-12-05 08:45:00' },
+    { id: 9, name: 'Nina Hoffmann', role: 'Marketing Specialist', email: 'n.hoffmann@firma.com', salary: 2950.00, gender: 'female', hire_date: '2023-09-22 09:30:00' },
+    { id: 10, name: 'Peter König', role: 'Projektleiter', email: 'p.koenig@firma.com', salary: 4000.00, gender: 'male', hire_date: '2021-10-10 08:00:00' },
+    { id: 11, name: 'Sara Lehmann', role: 'UX/UI Designerin', email: 's.lehmann@firma.com', salary: 3100.00, gender: 'female', hire_date: '2024-05-17 10:20:00' }
+];
+module.exports = { workers };
